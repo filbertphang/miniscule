7f56e6461cf930eb418d605f6177102906187472
filbert <fillberth@gmail.com>
2020-09-04 00:18:21 +0800

    moved scripts to assets/js, added animation for navbar height and border

---
+++ assets/js/nav.js
@@ -0,0 +1,20 @@
+var header = document.getElementsByClassName("site-header")[0]
+
+console.log(header)
+
+const storeScroll = () => {
+    if (window.pageYOffset > 0) {
+        header.classList.add('shadow');
+        if (header.id) {
+            header.classList.remove('fat');
+        }
+    }
+    else {
+        header.classList.remove('shadow');
+        if (header.id) {
+            header.classList.add('fat');
+        }
+    }
+}
+document.addEventListener('scroll', storeScroll);
+storeScroll()
