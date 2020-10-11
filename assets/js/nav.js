b1c3d308b2558501a9c98db6fe77c3b00439b5dd
filbert phang <fillberth@gmail.com>
2020-10-12 00:25:12 +0800

    added active sections to navbar based on scroll position

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,3 +1,5 @@
+---
+---
 var header = document.getElementsByClassName("site-header")[0]
 
 const storeScroll = () => {
@@ -17,3 +19,24 @@ const storeScroll = () => {
 document.addEventListener('scroll', storeScroll);
 storeScroll()
 
+var ids = []
+{% for feature in site.features %}
+ids.push("#{{ feature.id }}".replace(/\//g, "\\/"))
+{%- endfor %}
+
+window.addEventListener('DOMContentLoaded', () => {
+    window.addEventListener('scroll', () => {
+        for (j = 0; j < ids.length; j++){
+        cur_scroll = window.scrollY + 100
+        cur_elem = document.querySelector(ids[j])
+        mystr = 'a[href="' + ids[j] + '"]'
+
+        if (cur_scroll >= cur_elem.offsetTop && cur_scroll < (cur_elem.offsetTop + cur_elem.offsetHeight)){
+            document.querySelector(mystr).classList.add('active')
+        }
+        else{
+            document.querySelector(mystr).classList.remove('active')
+        }
+    }
+    })
+})
